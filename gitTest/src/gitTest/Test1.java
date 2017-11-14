62fede5f2c13df80e895aee7455799206d5a38ac
LGE\sy3.lee <sy3.lee@lge.com>
2017-11-14 11:39:56 +0900

    dbimpl commit2

--- gitTest/src/gitTest/Test1.java
+++ gitTest/src/gitTest/Test1.java
@@ -4,5 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Test1");
 		System.out.println("Develop Change");
+		System.out.println("Hello Test1");
+		System.out.println("Hello Test1");
 	}
 }
--- gitTest/src/gitTest/Test2.java
+++ gitTest/src/gitTest/Test2.java
@@ -4,5 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Test2");
 		System.out.println("Develop Change");
+		System.out.println("Hello Test2");
+		System.out.println("Hello Test2");
 	}
 }
