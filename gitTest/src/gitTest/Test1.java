e9819d8724779a114ec682cf349611d59d30f358
LGE\sy3.lee <sy3.lee@lge.com>
2017-11-14 10:37:56 +0900

    gitTest

---
+++ gitTest/src/gitTest/Test1.java
@@ -0,0 +1,7 @@
+package gitTest;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("Hello Test1");
+	}
+}
---
+++ gitTest/src/gitTest/Test2.java
@@ -0,0 +1,7 @@
+package gitTest;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("Hello Test2");
+	}
+}
