04cccdf9b5c31abf9518c4d421363ccf86d8450c
LGE\sy3.lee <sy3.lee@lge.com>
2017-11-14 10:58:15 +0900

    develop commit2

--- gitTest/src/gitTest/Test1.java
+++ gitTest/src/gitTest/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Test1");
 		System.out.println("Hello Test1");
+		System.out.println("Develop edit");
 	}
 }
--- gitTest/src/gitTest/Test2.java
+++ gitTest/src/gitTest/Test2.java
@@ -3,5 +3,6 @@ package gitTest;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Test2");
+		System.out.println("Develop edit2");
 	}
 }
