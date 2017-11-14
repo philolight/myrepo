2f84d510e54c5d05a094cee3fb3fa34003b8c20a
LGE\sy3.lee <sy3.lee@lge.com>
2017-11-14 13:46:24 +0900

    dbimpl develop rebased

--- gitTest/src/gitTest/Test1.java
+++ gitTest/src/gitTest/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Test1");
 		System.out.println("Develop Change");
-		System.out.println("Hello Test1");
-		System.out.println("Hello Test1");
+		System.out.println("dbimpl change1");
+		System.out.println("dbimpl change2");
 	}
 }
--- gitTest/src/gitTest/Test2.java
+++ gitTest/src/gitTest/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Test2");
 		System.out.println("Develop Change");
-		System.out.println("Hello Test2");
-		System.out.println("Hello Test2");
+		System.out.println("dbimpl change1");
+		System.out.println("dbimpl change2");
 	}
 }
